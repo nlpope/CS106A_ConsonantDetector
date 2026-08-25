e99344efc33f122b2678ad295d4960ae3bdafdb8
Noah Pope <npope@tutanota.com>
2026-08-25 07:23:59 -0500

    adding delimiters for StringTokenizer

--- CS106A_ConsonantDetector.java
+++ CS106A_ConsonantDetector.java
@@ -6,12 +6,20 @@
  * returns the consonants in uppercase form.
  * 
  * Attributions:
+ * The isEnglishVowel method was created by Eric
+ * Roberts' text 'The art and science of java' pg.
+ * 263.
  */
 
 import acm.program.*;
+import java.util.*;
 
 public class CS106A_ConsonantDetector extends ConsoleProgram
 {
+	/** Named constnts */
+	private static final String 
+	DELIMITERS = "!@#$%^&*()_-+={[}]:;\"'<,>.?/~`/\\ ";
+	
 	public void run()
 	{
 		println("This program takes a string and returns" +
@@ -24,12 +32,33 @@ public class CS106A_ConsonantDetector extends ConsoleProgram
 	private String findConsonants(String str)
 	{
 		//find tokens (word units)
+		StringTokenizer tokenizedStr = new StringTokenizer(
+				str.toUpperCase(), DELIMITERS, true	
+		);
 		String result = "";
-		String comparator = str.toLowerCase();
+		while(tokenizedStr.hasMoreTokens()){
+			
+			println(tokenizedStr.nextToken());
+			//result += tokenizedStr.nextToken();
+		}
 		
-		return "".toUpperCase();
+		return result;
 	}
 	
 	
-	private boolean
+	private boolean isWord(String str)
+	{
+		return true;
+	}
+	
+	
+	private boolean isEnglishVowel(char ch)
+	{
+		switch (Character.toLowerCase(ch)) {
+			case 'a': case 'e': case 'i': case 'o': case 'u':
+				return true;
+			default: 
+					return false;
+		}
+	}
 }
